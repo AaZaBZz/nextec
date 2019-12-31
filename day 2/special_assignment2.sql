dc4b2423258a1547aef168416aafe008f3c41d23
AaZaBZz <59351783+AaZaBZz@users.noreply.github.com>
2019-12-31 13:18:32 +0530

    Add files via upload

---
+++ day 2/special_assignment2.sql
@@ -0,0 +1,142 @@
+SQL> select last_name, hire_date, to_char(hire_date,'Day') day from employees order by to_char(hire_date -1, 'd');
+
+LAST_NAME                 HIRE_DATE DAY                                         
+------------------------- --------- ---------                                   
+Ladwig                    14-JUL-03 Monday                                      
+Cambrault                 15-OCT-07 Monday                                      
+Mallin                    14-JUN-04 Monday                                      
+Ernst                     21-MAY-07 Monday                                      
+Greene                    19-MAR-07 Monday                                      
+Banda                     21-APR-08 Monday                                      
+Walsh                     24-APR-06 Monday                                      
+Ande                      24-MAR-08 Monday                                      
+Vollman                   10-OCT-05 Monday                                      
+Kumar                     21-APR-08 Monday                                      
+Olson                     10-APR-07 Tuesday                                     
+
+LAST_NAME                 HIRE_DATE DAY                                         
+------------------------- --------- ---------                                   
+Urman                     07-MAR-06 Tuesday                                     
+Hunold                    03-JAN-06 Tuesday                                     
+King                      17-JUN-03 Tuesday                                     
+Hartstein                 17-FEB-04 Tuesday                                     
+Feeney                    23-MAY-06 Tuesday                                     
+Gates                     11-JUL-06 Tuesday                                     
+Chung                     14-JUN-05 Tuesday                                     
+Sarchand                  27-JAN-04 Tuesday                                     
+Taylor                    24-JAN-06 Tuesday                                     
+Abel                      11-MAY-04 Tuesday                                     
+Fox                       24-JAN-06 Tuesday                                     
+
+LAST_NAME                 HIRE_DATE DAY                                         
+------------------------- --------- ---------                                   
+Zlotkey                   29-JAN-08 Tuesday                                     
+Whalen                    17-SEP-03 Wednesday                                   
+Philtanker                06-FEB-08 Wednesday                                   
+Perkins                   19-DEC-07 Wednesday                                   
+Bell                      04-FEB-04 Wednesday                                   
+Kochhar                   21-SEP-05 Wednesday                                   
+Cabrio                    07-FEB-07 Wednesday                                   
+Chen                      28-SEP-05 Wednesday                                   
+Himuro                    15-NOV-06 Wednesday                                   
+Marlow                    16-FEB-05 Wednesday                                   
+Fay                       17-AUG-05 Wednesday                                   
+
+LAST_NAME                 HIRE_DATE DAY                                         
+------------------------- --------- ---------                                   
+Gee                       12-DEC-07 Wednesday                                   
+Partners                  05-JAN-05 Wednesday                                   
+Matos                     15-MAR-06 Wednesday                                   
+Stiles                    26-OCT-05 Wednesday                                   
+Lorentz                   07-FEB-07 Wednesday                                   
+Kaufling                  01-MAY-03 Thursday                                    
+Mikkilineni               28-SEP-06 Thursday                                    
+Patel                     06-APR-06 Thursday                                    
+Errazuriz                 10-MAR-05 Thursday                                    
+Bernstein                 24-MAR-05 Thursday                                    
+Olsen                     30-MAR-06 Thursday                                    
+
+LAST_NAME                 HIRE_DATE DAY                                         
+------------------------- --------- ---------                                   
+Sully                     04-MAR-04 Thursday                                    
+Smith                     10-MAR-05 Thursday                                    
+Doran                     15-DEC-05 Thursday                                    
+Marvins                   24-JAN-08 Thursday                                    
+Bloom                     23-MAR-06 Thursday                                    
+Grant                     24-MAY-07 Thursday                                    
+Fleaur                    23-FEB-06 Thursday                                    
+Sullivan                  21-JUN-07 Thursday                                    
+Everett                   03-MAR-05 Thursday                                    
+OConnell                  21-JUN-07 Thursday                                    
+Tuvault                   23-NOV-07 Friday                                      
+
+LAST_NAME                 HIRE_DATE DAY                                         
+------------------------- --------- ---------                                   
+Popp                      07-DEC-07 Friday                                      
+Gietz                     07-JUN-02 Friday                                      
+King                      30-JAN-04 Friday                                      
+Russell                   01-OCT-04 Friday                                      
+Sewall                    03-NOV-06 Friday                                      
+Baer                      07-JUN-02 Friday                                      
+Rajs                      17-OCT-03 Friday                                      
+Vishney                   11-NOV-05 Friday                                      
+Higgins                   07-JUN-02 Friday                                      
+Ozer                      11-MAR-05 Friday                                      
+Colmenares                10-AUG-07 Friday                                      
+
+LAST_NAME                 HIRE_DATE DAY                                         
+------------------------- --------- ---------                                   
+Johnson                   04-JAN-08 Friday                                      
+Taylor                    24-MAR-06 Friday                                      
+Mourgos                   16-NOV-07 Friday                                      
+Faviet                    16-AUG-02 Friday                                      
+Smith                     23-FEB-07 Friday                                      
+Sciarra                   30-SEP-05 Friday                                      
+Mavris                    07-JUN-02 Friday                                      
+Cambrault                 09-DEC-06 Saturday                                    
+Hall                      20-AUG-05 Saturday                                    
+Davies                    29-JAN-05 Saturday                                    
+Rogers                    26-AUG-06 Saturday                                    
+
+LAST_NAME                 HIRE_DATE DAY                                         
+------------------------- --------- ---------                                   
+Bissot                    20-AUG-05 Saturday                                    
+Markle                    08-MAR-08 Saturday                                    
+Nayer                     16-JUL-05 Saturday                                    
+Baida                     24-DEC-05 Saturday                                    
+Raphaely                  07-DEC-02 Saturday                                    
+Greenberg                 17-AUG-02 Saturday                                    
+De Haan                   13-JAN-01 Saturday                                    
+Austin                    25-JUN-05 Saturday                                    
+Jones                     17-MAR-07 Saturday                                    
+McCain                    01-JUL-06 Saturday                                    
+Dilly                     13-AUG-05 Saturday                                    
+
+LAST_NAME                 HIRE_DATE DAY                                         
+------------------------- --------- ---------                                   
+Dellinger                 24-JUN-06 Saturday                                    
+Hutton                    19-MAR-05 Saturday                                    
+Bates                     24-MAR-07 Saturday                                    
+Lee                       23-FEB-08 Saturday                                    
+Pataballa                 05-FEB-06 Sunday                                      
+Tucker                    30-JAN-05 Sunday                                      
+Livingston                23-APR-06 Sunday                                      
+Weiss                     18-JUL-04 Sunday                                      
+Geoni                     03-FEB-08 Sunday                                      
+Bull                      20-FEB-05 Sunday                                      
+Grant                     13-JAN-08 Sunday                                      
+
+LAST_NAME                 HIRE_DATE DAY                                         
+------------------------- --------- ---------                                   
+Khoo                      18-MAY-03 Sunday                                      
+Landry                    14-JAN-07 Sunday                                      
+Atkinson                  30-OCT-05 Sunday                                      
+Tobias                    24-JUL-05 Sunday                                      
+Seo                       12-FEB-06 Sunday                                      
+Vargas                    09-JUL-06 Sunday                                      
+McEwen                    01-AUG-04 Sunday                                      
+Fripp                     10-APR-05 Sunday                                      
+
+107 rows selected.
+
+SQL> spool off
